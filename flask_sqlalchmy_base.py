307c115d7c42eaf927f480d7360ff493c993c13b
bashzcrypt <31955330+mangesh-trimbake@users.noreply.github.com>
2025-01-06 23:54:10 +0530

    defined base class

--- flask_sqlalchmy_base.py
+++ flask_sqlalchmy_base.py
@@ -1,4 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, Integer, DateTime, func
 
+# Initialize SQLAlchemy without an app
 db = SQLAlchemy()
 
+@as_declarative()
+class Base:
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
